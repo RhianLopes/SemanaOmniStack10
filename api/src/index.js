3e6c86b6f8adf30ad1e52d8668b8c8ae7cbb421e
Rhian Lopes da Costa <rhian.costa@cwi.com.br>
2020-01-19 19:10:58 -0300

    feat(BaseWebProject): add the base web project with react js

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-yv3gr.mongodb.net/w
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
